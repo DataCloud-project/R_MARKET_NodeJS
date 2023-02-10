a13a6b1cbcd59861274c618e2de671eeded6c319
Aimen Djari <aimen.djari@iex.ec>
2023-02-10 15:05:52 +0100

    Create https server
    Signed-off-by: Aimen Djari <aimen.djari@iex.ec>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-const dbConnect= require('./collections/contracts');
 const express = require ('express');
 const routes = require('./routes/route');
 const swaggerDocs = require('./swagger.js');
 const cors = require('cors');
 // const swaggerUi = require('swagger-ui-express'),
 // swaggerDocument = require('./swagger.json');
-
-dbConnect().then((resp)=> {
-    resp.find().toArray().then((data)=>{
-        console.log('Database connected!')
-    })
-})
-
 
 //var corsOptions = {
 //	origin: '*'
@@ -33,8 +25,20 @@ app.use(cors(
 app.use(express.json());
 app.use('/', routes);
 //app.listen(5000);
-app.listen(port, () => {
-    console.log(`Example app listening at http://127.0.0.1:${port}`)
+
+var fs = require('fs');
+var https = require('https');
+var privateKey  = fs.readFileSync('server.key', 'utf8');
+var certificate = fs.readFileSync('server.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate, passphrase: 'datacloud'};
+
+// your express configuration here
+
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+    console.log(`Example app listening at https://127.0.0.1:${port}`)
     swaggerDocs(app, port)
   })
 // app.use(
