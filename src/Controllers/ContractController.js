70aa566afab60df9abb6b3153e93eacb51113134
aimen-djari <aimen-djari@mbp-de-aimen>
2023-10-23 04:06:00 +0200

    fix bug
    Signed-off-by: aimen-djari <aimen-djari@mbp-de-aimen>

--- src/Controllers/ContractController.js
+++ src/Controllers/ContractController.js
@@ -3,25 +3,18 @@ exports.getList = async (req, resp) => {
 	const requesterAddress = req.query.userAddress;
 	const dealsDataFromAPI = await axios.get('http://10.0.0.5:3000/deals?chainId=65535&requester='.concat(requesterAddress));
 	var result = dealsDataFromAPI.data.deals;
+
+
 	var array = new Array();
 	for (var i = 0; i < result.length; ++i) {
-		let unix_timestamp = result[i]['startTime'];
-		var date = new Date(unix_timestamp * 1000);
-		var hours = date.getHours();
-		var minutes = "0" + date.getMinutes();
-		var seconds = "0" + date.getSeconds();
-		
-		var day = date.getDate();
-		var month = date.getMonth() + 1;
-		var year = date.getFullYear();
-		
-		var formattedTime = day + '/' + month + '/' + year + ', ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2) + ' GMT';
 
 		var jsonObject =
 		{
 			contractID: result[i]['dealid'],
-			startTime: formattedTime
+			startTime: result[i]['startTime']
 		}
+
+
 		array.push(JSON.stringify(jsonObject, null, "\t"));
 	}
 	resp.send(array);
